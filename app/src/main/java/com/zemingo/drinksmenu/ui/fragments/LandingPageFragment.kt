3668f26929227b2b406cc68ff109a7a1e0513b4e
YanivSos <Yaniv.s@zemingo.com>
2020-05-01 15:09:19 +0300

    better code

--- app/src/main/java/com/zemingo/drinksmenu/ui/fragments/LandingPageFragment.kt
+++ app/src/main/java/com/zemingo/drinksmenu/ui/fragments/LandingPageFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
 import com.zemingo.drinksmenu.R
 import com.zemingo.drinksmenu.extensions.dpToPx
 import com.zemingo.drinksmenu.ui.SpacerItemDecoration
+import com.zemingo.drinksmenu.ui.models.DrinkPreviewUiModel
 import com.zemingo.drinksmenu.ui.models.LandingPageUiModel
 import com.zemingo.drinksmenu.ui.view_model.LandingPageViewModel
 import kotlinx.android.synthetic.main.fragment_landing_page.*
@@ -27,12 +29,20 @@ class LandingPageFragment : Fragment(R.layout.fragment_landing_page) {
 
     private fun setupAdapters() {
         val divider = SpacerItemDecoration(right = 16.dpToPx().toInt())
-        latest_arrivals_rv.adapter = latestArrivalsAdapter
-        latest_arrivals_rv.addItemDecoration(divider)
-        most_popular_rv.adapter = mostPopularAdapter
-        most_popular_rv.addItemDecoration(divider)
-        recent_searches_rv.adapter = recentSearchesAdapter
-        recent_searches_rv.addItemDecoration(divider)
+        initDrinkPreviewRecyclerView(latest_arrivals_rv, latestArrivalsAdapter, divider)
+        initDrinkPreviewRecyclerView(most_popular_rv, mostPopularAdapter, divider)
+        initDrinkPreviewRecyclerView(recent_searches_rv, recentSearchesAdapter, divider)
+    }
+
+    private fun initDrinkPreviewRecyclerView(
+        recyclerView: RecyclerView,
+        adapter: DrinkPreviewAdapter,
+        itemDecoration: RecyclerView.ItemDecoration
+    ) {
+        recyclerView.run {
+            this.adapter = adapter.apply { onClick = { onDrinkPreviewClicked(it) } }
+            addItemDecoration(itemDecoration)
+        }
     }
 
     private fun observeLandingPage() {
@@ -47,5 +57,9 @@ class LandingPageFragment : Fragment(R.layout.fragment_landing_page) {
             mostPopularAdapter.update(mostPopular)
             recentSearchesAdapter.update(recentSearches)
         }
+    }
+
+    private fun onDrinkPreviewClicked(drinkPreviewUiModel: DrinkPreviewUiModel) {
+
     }
 }
