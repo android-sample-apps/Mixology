357120f14d643619df4fc032a2a83bc72f959259
YanivSos <Yaniv.s@zemingo.com>
2020-05-01 16:59:49 +0300

    WIP: collapsing toolbar

--- app/src/main/java/com/zemingo/drinksmenu/ui/fragments/DrinkFragment.kt
+++ app/src/main/java/com/zemingo/drinksmenu/ui/fragments/DrinkFragment.kt
@@ -2,12 +2,10 @@ package com.zemingo.drinksmenu.ui.fragments
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
-import coil.api.load
 import com.zemingo.drinksmenu.R
 import com.zemingo.drinksmenu.domain.models.DrinkModel
 import com.zemingo.drinksmenu.extensions.fromLink
@@ -33,6 +31,9 @@ class DrinkFragment : Fragment(R.layout.fragment_drink_collapsing) {
     }
 
     private fun initToolbar() {
+//        drink_app_bar.doOnPreDraw {
+//            it.layoutParams.height = requireView().height / 3
+//        }
     }
 
     private fun observeDrink() {
@@ -42,7 +43,8 @@ class DrinkFragment : Fragment(R.layout.fragment_drink_collapsing) {
     }
 
     private fun showDrinkOnScreen(drinkModel: DrinkModel) {
-//        drink_header_image.fromLink(drinkModel.thumbnail)
+        drink_header_image.fromLink(drinkModel.thumbnail)
+        drink_toolbar.title = drinkModel.name
         drink_tv.text = drinkModel.toStringUi() + "\n" + drinkModel.toStringUi()
     }
 }
