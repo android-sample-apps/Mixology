bf74496978a6686a1e9302331fd6220c5e958ee9
YanivSos <Yaniv.s@zemingo.com>
2020-05-02 19:09:33 +0300

    rename fragment

--- app/src/main/java/com/zemingo/drinksmenu/ui/fragments/DrinkFragment.kt
+++ app/src/main/java/com/zemingo/drinksmenu/ui/fragments/DrinkFragment.kt
@@ -2,8 +2,6 @@ package com.zemingo.drinksmenu.ui.fragments
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.tabs.TabLayoutMediator
@@ -11,13 +9,10 @@ import com.zemingo.drinksmenu.R
 import com.zemingo.drinksmenu.domain.models.DrinkModel
 import com.zemingo.drinksmenu.extensions.fromLink
 import com.zemingo.drinksmenu.ui.adapters.DrinkPagerAdapter
-import com.zemingo.drinksmenu.ui.view_model.DrinkViewModel
-import kotlinx.android.synthetic.main.fragment_drink_collapsing.*
-import org.koin.android.viewmodel.ext.android.sharedViewModel
-import org.koin.android.viewmodel.ext.android.viewModel
+import kotlinx.android.synthetic.main.fragment_drink.*
 
 
-class DrinkFragment : BaseDrinkFragment(R.layout.fragment_drink_collapsing) {
+class DrinkFragment : BaseDrinkFragment(R.layout.fragment_drink) {
 
     private val args: DrinkFragmentArgs by navArgs()
     private val pagerAdapter: DrinkPagerAdapter by lazy { DrinkPagerAdapter(this) }
