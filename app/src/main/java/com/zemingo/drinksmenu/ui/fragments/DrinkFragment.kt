ab0f0e17f436857c482824ea1a8c598a38eb5654
YanivSos <Yaniv.s@zemingo.com>
2020-05-03 17:13:07 +0300

    remove dead code

--- app/src/main/java/com/zemingo/drinksmenu/ui/fragments/DrinkFragment.kt
+++ app/src/main/java/com/zemingo/drinksmenu/ui/fragments/DrinkFragment.kt
@@ -1,18 +1,13 @@
 package com.zemingo.drinksmenu.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.tabs.TabLayoutMediator
 import com.zemingo.drinksmenu.R
-import com.zemingo.drinksmenu.extensions.clearTranslucentStatusBar
 import com.zemingo.drinksmenu.extensions.fromLink
-import com.zemingo.drinksmenu.extensions.translucentStatusBar
 import com.zemingo.drinksmenu.ui.adapters.DrinkPagerAdapter
 import com.zemingo.drinksmenu.ui.models.DrinkUiModel
-import com.zemingo.drinksmenu.ui.utils.AppBarStateChangeListener
 import com.zemingo.drinksmenu.ui.view_model.DrinkViewModel
 import kotlinx.android.synthetic.main.fragment_drink_expanded.*
 import kotlinx.android.synthetic.main.layout_drink_label.*
@@ -29,47 +24,9 @@ class DrinkFragment : BaseDrinkFragment(R.layout.fragment_drink_expanded) {
         return getViewModel { parametersOf(args.id) }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-//        translucentStatusBar()
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
-    override fun onDestroyView() {
-//        clearTranslucentStatusBar()
-        super.onDestroyView()
-    }
-
-    private fun translucentStatusBar() {
-        requireActivity().translucentStatusBar()
-    }
-
-    private fun clearTranslucentStatusBar() {
-        requireActivity().clearTranslucentStatusBar()
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initAppBar()
         initInfoPagerAdapter()
-    }
-
-    private fun initAppBar() {
-        /*drink_app_bar.addOnOffsetChangedListener(
-            AppBarStateChangeListener { _, state ->
-                onAppBarStateChanged(state)
-            })*/
-    }
-
-    private fun onAppBarStateChanged(state: AppBarStateChangeListener.State) {
-        if (state == AppBarStateChangeListener.State.EXPANDED) {
-            translucentStatusBar()
-        } else if (state == AppBarStateChangeListener.State.COLLAPSED) {
-            clearTranslucentStatusBar()
-        }
     }
 
     private fun initInfoPagerAdapter() {
