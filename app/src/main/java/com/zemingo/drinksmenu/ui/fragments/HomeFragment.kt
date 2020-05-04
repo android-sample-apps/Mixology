1275aa385fa5f8720a1c4cfde546c13ed14e001e
YanivSos <Yaniv.s@zemingo.com>
2020-05-04 11:43:49 +0300

    keep tab position

--- app/src/main/java/com/zemingo/drinksmenu/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/zemingo/drinksmenu/ui/fragments/HomeFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
 import com.zemingo.drinksmenu.R
 import com.zemingo.drinksmenu.ui.adapters.HomePagerAdapter
+import com.zemingo.drinksmenu.ui.utils.SimpleOnTabSelectedListener
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var itemPosition = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,7 +27,13 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 tab.text = getString(homeAdapter.title(position))
                 setCurrentItem(position, true)
             }.attach()
-            currentItem = 0
+
+            home_tabs.addOnTabSelectedListener(SimpleOnTabSelectedListener(
+                onSelected = { tab ->
+                    itemPosition = tab.position
+                }
+            ))
+            currentItem = itemPosition
         }
     }
 }
---
+++ app/src/main/java/com/zemingo/drinksmenu/ui/utils/SimpleOnTabSelectedListener.kt
@@ -0,0 +1,22 @@
+package com.zemingo.drinksmenu.ui.utils
+
+import com.google.android.material.tabs.TabLayout
+
+class SimpleOnTabSelectedListener(
+    private val onReSelected: ((TabLayout.Tab) -> Unit)? = null,
+    private val onSelected: ((TabLayout.Tab) -> Unit)? = null,
+    private val onUnSelected: ((TabLayout.Tab) -> Unit)? = null
+): TabLayout.OnTabSelectedListener {
+
+    override fun onTabReselected(tab: TabLayout.Tab) {
+        onReSelected?.invoke(tab)
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab) {
+        onUnSelected?.invoke(tab)
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab) {
+        onSelected?.invoke(tab)
+    }
+}
