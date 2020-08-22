d19c763c12b02b760aa30a00a9a91fafcea6f808
yanivsos <yanivsosnovsky@gmail.com>
2020-08-22 18:15:31 +0300

    surround 'launchInAppReview' with try/catch

--- app/src/main/java/com/yanivsos/mixological/in_app_review/RequestInAppReviewUseCase.kt
+++ app/src/main/java/com/yanivsos/mixological/in_app_review/RequestInAppReviewUseCase.kt
@@ -4,15 +4,20 @@ import android.app.Activity
 import com.google.android.play.core.ktx.launchReview
 import com.google.android.play.core.ktx.requestReview
 import com.google.android.play.core.review.ReviewManager
+import timber.log.Timber
 
 class RequestInAppReviewUseCase(
     private val reviewManager: ReviewManager
 ) {
 
     suspend fun launchReview(activity: Activity) {
-        reviewManager.run {
-            val reviewInfo = requestReview()
-            launchReview(activity, reviewInfo)
+        try {
+            reviewManager.run {
+                val reviewInfo = requestReview()
+                launchReview(activity, reviewInfo)
+            }
+        } catch (e: Exception) {
+            Timber.e(e, "unable to launch review")
         }
     }
 }
