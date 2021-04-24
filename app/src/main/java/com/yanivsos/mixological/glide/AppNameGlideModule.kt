e7d3d60f4a59c68f396b34fa3de73305ff5f1a10
yanivsos <yanivsosnovsky@gmail.com>
2021-04-24 19:07:54 +0300

    use applications OkHttpClient for glide

--- app/src/main/java/com/yanivsos/mixological/glide/AppNameGlideModule.kt
+++ app/src/main/java/com/yanivsos/mixological/glide/AppNameGlideModule.kt
@@ -1,7 +1,27 @@
 package com.yanivsos.mixological.glide
 
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
+import okhttp3.OkHttpClient
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import java.io.InputStream
 
 @GlideModule
-open class AppNameGlideModule: AppGlideModule()
+open class AppNameGlideModule: AppGlideModule(), KoinComponent {
+
+    private val okHttpClient:OkHttpClient by inject()
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        registry.replace(
+            GlideUrl::class.java,
+            InputStream::class.java,
+            OkHttpUrlLoader.Factory(okHttpClient)
+        )
+    }
+}
