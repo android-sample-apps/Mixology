f35ac697f0809696b21f70fbdc6b0c7d445ab4fd
yanivsos <yanivsosnovsky@gmail.com>
2021-04-30 19:03:03 +0300

    remove todo

--- app/src/main/java/com/yanivsos/mixological/database/Converters.kt
+++ app/src/main/java/com/yanivsos/mixological/database/Converters.kt
@@ -9,13 +9,11 @@ class MapStringToStringOptionalConverter {
 
     @TypeConverter
     fun fromMap(map: Map<String, String?>): String {
-        // TODO: 21/04/2021 test this
         return Json.encodeToString(map)
     }
 
     @TypeConverter
     fun toMap(map: String): Map<String, String?> {
-        // TODO: 21/04/2021 test this
         return Json.decodeFromString(map)
     }
 }
