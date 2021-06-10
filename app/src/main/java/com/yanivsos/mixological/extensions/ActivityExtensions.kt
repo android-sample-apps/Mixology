366b07dd4c359d3bd6890cfd84fe90b2ccace653
yanivsos <yanivsosnovsky@gmail.com>
2021-06-10 19:43:42 +0300

    replace deprecated method

--- app/src/main/java/com/yanivsos/mixological/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/yanivsos/mixological/extensions/ActivityExtensions.kt
@@ -4,15 +4,12 @@ import android.app.Activity
 import androidx.core.app.ShareCompat
 import com.yanivsos.mixological.ui.models.DrinkUiModel
 
-
 fun Activity.shareDrink(drinkUiModel: DrinkUiModel) {
-    ShareCompat.IntentBuilder
-        .from(this)
+    ShareCompat.IntentBuilder(this)
         .setText(drinkUiModel.shareText)
         .setSubject(drinkUiModel.name)
         // most general text sharing MIME type
         .setType("text/plain")
-
         .setChooserTitle(drinkUiModel.name)
         .startChooser()
 }
